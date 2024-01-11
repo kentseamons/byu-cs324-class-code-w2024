64484baeba1dd9ff762b7f8a4396fede1619cf4f
Kent Seamons <seamons@keep.cs.byu.edu>
2024-01-11 04:40:56 -0700

    add argv.c

--- week1/argv.c
+++ week1/argv.c
@@ -4,16 +4,20 @@
  * gcc -g -o argv argv.c
  */
 
-// an equivalent definition for main
+// an equivalent definition for main function using char **argv
 // int main ( int argc, char **argv) {
 
 int main( int argc, char *argv[] )  {
 
     printf("Value of argc is %d\n",argc);
     printf("Address of argc is %p\n",&argc);
+
+    printf("Value of argv is %p\n",argv);
+    printf("Address of argv is %p\n",&argv);
     
     for (int i=0; i<argc; i++) {
         
-        printf("Argument %d: %s\n", i, argv[i]);
+        printf("Value of argv[%d]: %s\n", i, argv[i]);
+        printf("Address of argv[%d]: %p\n", i, &argv[i]);
     }
 }
