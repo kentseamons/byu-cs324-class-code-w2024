99c5369ef878ebdbf00872e8f925ea29826ec6bb
Kent Seamons <seamons@fortress.cs.byu.edu>
2024-01-13 19:43:36 -0700

    update argv.c

--- week1/argv.c
+++ week1/argv.c
@@ -10,14 +10,14 @@
 int main( int argc, char *argv[] )  {
 
     printf("Value of argc is %d\n",argc);
-    printf("Address of argc is %p\n",&argc);
+    printf("Address of argc is %p\n\n",&argc);
 
     printf("Value of argv is %p\n",argv);
     printf("Address of argv is %p\n",&argv);
     
     for (int i=0; i<argc; i++) {
-        
-        printf("Value of argv[%d]: %s\n", i, argv[i]);
         printf("Address of argv[%d]: %p\n", i, &argv[i]);
+        printf("Value stored in argv[%d]: %p - this is a pointer stored in the argv array\n", i, argv[i]);
+        printf("String referenced by argv[%d]: %s\n", i, argv[i]);
     }
 }
